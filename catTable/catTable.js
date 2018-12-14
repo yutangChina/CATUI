dc941d2906f8d84ebb32d5d843a989f05dca6f99
Carlos Tang <tangyu_nju@163.com>
2018-12-14 20:31:43 +0800

    Update catTable.js

--- catTable/catTable.js
+++ catTable/catTable.js
@@ -4,14 +4,25 @@
  * Date 2018-11-28
  */
 /**
+ * catTbale v1.1.0
+ * Author Tang Yu
+ * Date 2018-12-14
+ * 知识点: 
+ * 1.rowspan 行合并; colspan 列合并; 一行还是一行来看
+ * 新增功能:
+ * 1.新增多层表头的展示
+ * 问题修复:
+ * 1.修复多个表格的时候，只有第一个表格的分页组件可以使用的bug
+ */
+/**
  * 未兼容IE8以下的方法：querySelector
  * 
- * rowspan='3'
  */
 jQuery.fn.catTable = function (obj) {
     /**----------------------------参数初始化定义区-------------------------------------------------------------*/
     var _tableDomTree = null; //table实际的dom树 内部使用
     var nowIndex = 0; //初始数组起始位置  当dataFrom == Local的时候启用
+    var _targetDiv = this[0]; //组件依附的div
 
     /**   表格头有关的配置项  **/
     var thStyle = {}; //表格头的渲染格式 可以使对象 表示全都用他，可以使数组分别渲染，为数组的时候数组的长度必须等于thead的个数
@@ -138,47 +149,163 @@ jQuery.fn.catTable = function (obj) {
      * @param {*} _thshowType 
      */
     function _handleHeadToVirDom() {
+        //表头的基础虚拟dom
         var _headVirDom = {
             tagName: 'thead',
             props: {
 
             },
-            children: [{
+            children: []
+        };
+        //表头数组
+        var _trArr = [];
+        //表头层级
+        var _level = 0;
+        //表头的虚拟dom生成函数
+        _multiHeadToVirDom(thead, _trArr, _level, null);
+        //当thead为对象的时候，可能是多层表头，要进行相应处理
+        if (_thshowType == 'Object') {
+            _handleMultiHead(_trArr);
+        }
+
+
+        //_thStyleType样式只对单层表头有效
+        if (_trArr.length == 1) {
+            //一个tr表示是单层表头
+            for (var i = 0; i < _trArr[0].children.length; i++) {
+                var _tempTh = _trArr[0].children[i];
+
+                switch (_thStyleType) {
+                    case 'None':
+                        break;
+                    case 'Single':
+                        if (thStyle[i] != undefined) {
+                            for (var o in thStyle[i]) {
+                                if (_tempTh['props'][o] != undefined) {
+                                    _tempTh['props'][o] += ' ' + thStyle[i][o];
+                                } else {
+                                    _tempTh['props'][o] = thStyle[i][o];
+                                }
+                            }
+
+                        }
+                        break;
+                    case 'All':
+                        for (var o in thStyle) {
+                            if (_tempTh['props'][o] != undefined) {
+                                _tempTh['props'][o] += ' ' + thStyle[o];
+                            } else {
+                                _tempTh['props'][o] = thStyle[o];
+                            }
+                        }
+                        break;
+                }
+
+            }
+        }
+        _headVirDom.children = _trArr;
+        return _headVirDom;
+    }
+    /**
+     * 表头的虚拟dom生成函数
+     * @param {*} _multiHead 
+     * @param {*} _trArr 
+     * @param {*} _level 
+     * @param {*} _belong 
+     */
+    function _multiHeadToVirDom(_multiHead, _trArr, _level, _belong) {
+        if (_trArr[_level] == undefined) {
+            _trArr[_level] = {
                 tagName: 'tr',
                 props: {
 
                 },
                 children: []
-            }]
-        };
-        var _children = _headVirDom.children[0].children;
-        for (var i = 0; i < thead.length; i++) {
-            var thText = '';
-            if (_thshowType == 'String') {
-                thText = thead[i];
-            } else {
-                thText = thead[i][thField[0]];
-                _dataShowOrder.push(thead[i][thField[1]]);
-            }
-            var _tempTh = {
-                tagName: 'th',
-                children: [thText]
             };
-            switch (_thStyleType) {
-                case 'None':
-                    _tempTh['props'] = {};
-                    break;
-                case 'Single':
-                    _tempTh['props'] = thStyle[i] == undefined ? {} : thStyle[i];
-                    break;
-                case 'All':
-                    _tempTh['props'] = thStyle;
-                    break;
-
+        }
+        var _children = _trArr[_level]['children'];
+        if (_thshowType == 'String') {
+            for (var i = 0; i < _multiHead.length; i++) {
+                var _tempTh = {
+                    tagName: 'th',
+                    children: [_multiHead[i]]
+                };
+                _children.push(_tempTh);
             }
-            _children.push(_tempTh);
-        };
-        return _headVirDom;
+        } else {
+            for (var i = 0; i < _multiHead.length; i++) {
+                //展示的文字
+                var _tempDisplay = _multiHead[i][thField[0]];
+                //代表的字段code
+                var _tempCode = _multiHead[i][thField[1]];
+                //虚拟dom
+                var _temp = {
+                    tagName: 'th',
+                    props: {
+                        'catfield': _tempCode
+                    },
+                    children: [_tempDisplay]
+                };
+                //是否有归属
+                if (_belong != null || _belong != undefined) {
+                    _temp['props']['belong'] = _belong;
+
+                }
+                _children.push(_temp);
+                //是否有下一层
+                if (_multiHead[i].children != undefined) {
+                    _multiHeadToVirDom(_multiHead[i].children, _trArr, (_level + 1), _tempCode);
+                } else {
+                    //没有下一层，则入数据展示字段信息数组
+                    _dataShowOrder.push(_tempCode);
+                }
+            }
+        }
+    }
+    /**
+     * 处理对象类型表头，生成单层/多层表头
+     * @param {*} _trArr 
+     */
+    function _handleMultiHead(_trArr) {
+        //处理数据进行多层次表头的渲染
+        var _span = {};
+        var _trLen = _trArr.length;
+        //从最后开始，最后即为表头最底层的字段
+        for (var i = (_trLen - 1); i > -1; i--) {
+            //该行所拥有的th数组
+            var _tempTrArr = _trArr[i].children;
+            //该行所拥有的th数组的长度
+            var _tempTrLen = _tempTrArr.length;
+            for (var j = 0; j < _tempTrLen; j++) {
+                //th的虚拟dom
+                var _tempTh = _tempTrArr[j];
+                //该虚拟dom的props
+                var _tempProps = _tempTh.props;
+                //该虚拟dom所代表的字段
+                var _catfield = _tempProps['catfield'];
+                //该虚拟dom的父级字段
+                var _belong = _tempProps['belong'];
+                //该字段在_span中是否存在
+                if (_span[_catfield] == undefined) {
+                    _span[_catfield] = 1;
+                    //如果不存在则rowspan为当前实际的层级
+                    _tempProps['rowspan'] = _trLen - i;
+                } else {
+                    //如果存在则说明已经有下一级了，因此rowspan 为 1
+                    _tempProps['rowspan'] = 1;
+                }
+                //其父级字段是否存在
+                if (_belong != undefined) {
+                    if (_span[_belong] == undefined) {
+                        _span[_belong] = 1;
+                    } else {
+                        _span[_belong] += _span[_catfield];
+                    }
+                }
+                //添加对应的colspan属性
+                _tempProps['colspan'] = _span[_catfield];
+            }
+        }
     }
     /**
      * 生成tbody的虚拟dom  <tbody></tbody>
@@ -271,6 +398,9 @@ jQuery.fn.catTable = function (obj) {
         }
         var children = obj.children;
         for (var i = 0; i < children.length; i++) {
+            if (children[i] == undefined) {
+                continue;
+            }
             if (children[i].__proto__ == String.prototype) {
                 ele.appendChild(document.createTextNode(children[i]));
             } else {
@@ -299,7 +429,7 @@ jQuery.fn.catTable = function (obj) {
      * 添加上一步按钮事件
      */
     function _clickPreviousStep() {
-        var dom = document.querySelector('.first-child');
+        var dom = _targetDiv.querySelector('.first-child');
         this[0].addEventListener('click', function (e) {
             if (e.target == dom || dom.contains(e.target)) {
                 _previous();
@@ -310,7 +440,7 @@ jQuery.fn.catTable = function (obj) {
      * 添加下一步按钮事件
      */
     function _clickNextStep() {
-        var dom = document.querySelector('.last-child');
+        var dom = _targetDiv.querySelector('.last-child');
         this[0].addEventListener('click', function (e) {
             if (e.target == dom || dom.contains(e.target)) {
                 _next();
@@ -371,7 +501,7 @@ jQuery.fn.catTable = function (obj) {
      * 修改input框内的数字跳转到对应的页
      */
     function _changeInputIndexNum() {
-        var _inputPage = document.querySelector('.input-page-num');
+        var _inputPage = _targetDiv.querySelector('.input-page-num');
         this[0].addEventListener('change', function (e) {
             var _dom = e.target;
             if (_dom == _inputPage) {
@@ -424,7 +554,7 @@ jQuery.fn.catTable = function (obj) {
      * 上一页，下一页的时候修改页脚input框中显示的页数
      */
     function _changeInputPageNum() {
-        document.querySelector('.input-page-num').value = pageNumber + 1;
+        _targetDiv.querySelector('.input-page-num').value = pageNumber + 1;
     }
     /**
      * 根据数据生成footer的中间主体:1,2,3,4,...,7,9
